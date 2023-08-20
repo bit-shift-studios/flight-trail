dc6cea41e415ce7944fe2e078ef5411f198a0145
Xero kun <elo48858@gmail.com>
2023-08-20 18:08:51 +0100

    `FEAT` Implemented app database abstract class

--- app/src/main/java/bitshift/studios/flighttrail/data/db/AppDatabase.kt
+++ app/src/main/java/bitshift/studios/flighttrail/data/db/AppDatabase.kt
@@ -1,4 +1,22 @@
 package bitshift.studios.flighttrail.data.db
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import bitshift.studios.flighttrail.data.db.airport.AirportDao
+import bitshift.studios.flighttrail.data.db.airport.entities.AirportEntity
+import bitshift.studios.flighttrail.data.db.bookmarked.BookmarkedFlightDao
+import bitshift.studios.flighttrail.data.db.bookmarked.entities.BookmarkedFlightEntity
+
+@Database(
+	entities = [AirportEntity::class, BookmarkedFlightEntity::class],
+	exportSchema = false,
+	version = 2
+)
+abstract class AppDatabase : RoomDatabase() {
+	abstract fun airportDao(): AirportDao
+	abstract fun bookmarkedFlightDao(): BookmarkedFlightDao
+
+	companion object {
+		const val databaseName = "app_database"
+	}
 }
