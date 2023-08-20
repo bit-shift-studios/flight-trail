97283ccadf40c446257c920a882c32715264cd35
Xero kun <elo48858@gmail.com>
2023-08-20 16:33:24 +0100

    `FEAT` Implemented Airport Entity Data Access Object

--- app/src/main/java/bitshift/studios/flighttrail/data/db/airport/AirportDao.kt
+++ app/src/main/java/bitshift/studios/flighttrail/data/db/airport/AirportDao.kt
@@ -1,4 +1,18 @@
 package bitshift.studios.flighttrail.data.db.airport
 
-class AirportDao {
+import androidx.room.Dao
+import androidx.room.Query
+import bitshift.studios.flighttrail.data.db.airport.entities.AirportEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AirportDao {
+	@Query(
+		"""
+			SELECT * FROM airport WHERE LOWER(name) LIKE '%' || LOWER(:query) || '%' OR
+			LOWER(iata_code) LIKE '%' || LOWER(:query) || '%'
+			ORDER BY passengers DESC
+		"""
+	)
+	fun getAirportsByQuery(query: String): Flow<List<AirportEntity>>
 }
