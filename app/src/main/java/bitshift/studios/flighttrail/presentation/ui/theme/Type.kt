2c89c65e452b089f6e83e871d576dc05280ec6a5
Xero kun <elo48858@gmail.com>
2023-08-21 19:31:01 +0100

    `FEAT` Defined typefaces and typography

--- app/src/main/java/bitshift/studios/flighttrail/presentation/ui/theme/Type.kt
+++ app/src/main/java/bitshift/studios/flighttrail/presentation/ui/theme/Type.kt
@@ -2,33 +2,108 @@ package bitshift.studios.flighttrail.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import bitshift.studios.flighttrail.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-	bodyLarge = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp,
-		lineHeight = 24.sp,
-		letterSpacing = 0.5.sp
+val AvenirLT = FontFamily(
+	Font(
+		resId = R.font.avenir_black,
+		weight = FontWeight.Black,
+		style = FontStyle.Normal
+	),
+	Font(
+		resId = R.font.avenir_xbold,
+		weight = FontWeight.ExtraBold,
+		style = FontStyle.Normal
+	),
+	Font(
+		resId = R.font.avenir_normal,
+		weight = FontWeight.Normal,
+		style = FontStyle.Normal
 	)
-	/* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val Inter = FontFamily(
+	Font(
+		resId = R.font.inter_bold,
+		weight = FontWeight.Bold,
+		style = FontStyle.Normal
+	),
+	Font(
+		resId = R.font.inter_medium,
+		weight = FontWeight.Medium,
+		style = FontStyle.Normal
+	),
+	Font(
+		resId = R.font.inter_normal,
+		weight = FontWeight.Normal,
+		style = FontStyle.Normal
+	)
+)
+
+val Typography = Typography(
+	displayLarge = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.Black,
+		fontSize = 48.sp,
+		lineHeight = 60.sp
+	),
+	displayMedium = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.ExtraBold,
+		fontSize = 32.sp,
+		lineHeight = 50.sp
+	),
+	displaySmall = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.ExtraBold,
+		fontSize = 16.sp,
+		lineHeight = 24.sp
+	),
+	headlineMedium = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.ExtraBold,
+		fontSize = 18.sp,
+		lineHeight = 30.sp
+	),
+	headlineLarge = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.ExtraBold,
+		fontSize = 40.sp,
+		lineHeight = 52.sp
+	),
+	titleMedium = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Bold,
+		fontSize = 24.sp,
+		lineHeight = 36.sp
+	),
+	bodyMedium = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Medium,
+		fontSize = 18.sp,
+		lineHeight = 30.sp
+	),
+	bodyLarge = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Medium,
+		fontSize = 24.sp,
+		lineHeight = 36.sp
+	),
+	labelMedium = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.Black,
+		fontSize = 18.sp,
+		lineHeight = 30.sp
+	),
+	labelSmall = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.ExtraBold,
+		fontSize = 16.sp,
+		lineHeight = 28.sp
+	)
 )
