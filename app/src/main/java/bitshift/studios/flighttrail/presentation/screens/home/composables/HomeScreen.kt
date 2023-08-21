d2b0b958c75519781b81c93946422a26fb5b14fd
Xero kun <elo48858@gmail.com>
2023-08-22 00:26:58 +0100

    `FEAT` Created base home screen

---
+++ app/src/main/java/bitshift/studios/flighttrail/presentation/screens/home/composables/HomeScreen.kt
@@ -0,0 +1,25 @@
+package bitshift.studios.flighttrail.presentation.screens.home.composables
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+	modifier: Modifier = Modifier
+) {
+	val isDarkTheme = isSystemInDarkTheme()
+
+	Column(
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.Center,
+		modifier = modifier.fillMaxSize()
+	) {
+		Text(text = "Home screen")
+	}
+}
