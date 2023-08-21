409a8f7c6eba6f7b2ee85d7f91e812106f74e429
Xero kun <elo48858@gmail.com>
2023-08-21 19:02:54 +0100

    `FEAT` Defined color palette

--- app/src/main/java/bitshift/studios/flighttrail/presentation/ui/theme/Color.kt
+++ app/src/main/java/bitshift/studios/flighttrail/presentation/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package bitshift.studios.flighttrail.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Neutral900 = Color(0xFF090A0D)
+val Neutral800 = Color(0xFF16181E)
+val Neutral700 = Color(0xFF292B34)
+val Neutral600 = Color(0xFF292B34)
+val Neutral500 = Color(0xFFE8E9ED)
+val Neutral400 = Color(0xFFF7F9FB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Main100 = Color(0xFF277CFC)
+val Main090 = Color(0xFF619DF7)
+val Main080 = Color(0xFF8BBDEF)
+val Main070 = Color(0xFFD0E7FD)
